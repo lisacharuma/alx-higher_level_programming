456db7d5c741c3f3d20443300dc08d75e02081b0
lisacharuma <charumalisah@gmail.com>
2023-05-16 05:07:12 -0700

    replaces an element at a given index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,7 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if not isinstance(my_list, list) or
-    if not isinstance(idx, int):
+    if not isinstance(my_list, list):
         return (my_list)
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
