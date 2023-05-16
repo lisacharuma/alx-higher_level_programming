9cde252ab17f2e389bc34ba45fcebd50d2914dc4
lisacharuma <charumalisah@gmail.com>
2023-05-16 05:41:09 -0700

    A function that replaces an element at a given index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,6 +7,5 @@ def replace_in_list(my_list, idx, element):
         return (my_list)
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
-    rep = [i for i in my_list]
-    rep[idx] = element
-    return (rep)
+    my_list[idx] = element
+    return (my_list)
