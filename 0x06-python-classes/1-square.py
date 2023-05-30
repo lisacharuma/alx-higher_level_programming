076f1d358b28d1b4250c84bae371ee2f670342b5
lisacharuma <charumalisah@gmail.com>
2023-05-30 05:33:39 -0700

    Defines square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class square"""
+
+
+class Square:
+    """Square representation"""
+
+    def __init__(self, size):
+        """square initialization
+
+        Arg:
+        size(int): square size
+        """
+
+        self.__size = size
