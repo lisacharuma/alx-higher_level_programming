b7e2346f1b74d880661f1a41be080d627872c8a7
lisacharuma <charumalisah@gmail.com>
2023-05-30 12:37:44 -0700

    Squares a matrix and returns a new matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""squares a matrix"""
+
+
+def square_matrix_simple(matrix=[]):
+    res_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        res_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+    return (res_matrix)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,3 @@
+0x04. Python - More Data Structures: Set, Dictionary
+
+In this project, we are covering more data structures, with more focus on sets and dictionaries.
