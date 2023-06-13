5956a0629c1f4da421827ece8e6dd4103584a768
lisacharuma <charumalisah@gmail.com>
2023-06-13 05:03:21 -0700

    Fixed function by removing another prototype

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,5 +7,4 @@ def to_json_string(my_obj):
     """
     This function returns the JSON representation of an object
     """
-    def to_json_string(my_obj):
-        return (json.dumps(my_obj))
+    return (json.dumps(my_obj))
