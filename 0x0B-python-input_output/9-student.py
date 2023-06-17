841b02b5a03285c60c2e3ee7190cbcff78eab5b7
lisacharuma <charumalisah@gmail.com>
2023-06-16 18:32:39 -0700

    A class defining student object

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Student module"""
+
+
+class Student:
+    """Class representing a student"""
+    def __init__(self, first_name, last_name, age):
+        """New student obj initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def class_to_json(self):
+        """Returns a json rep of a student object"""
+        return (self.__dict__)
