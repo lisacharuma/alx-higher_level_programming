f8f7d1659fb8e70fe4852725fff6fd948e97cf4a
lisacharuma <charumalisah@gmail.com>
2023-06-17 23:27:01 -0700

    A function that appends text after a given string

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,11 +8,12 @@ def append_after(filename="", search_string="", new_string=""):
     search_string: string after which new strin is appended
     new_string: string to be appended
     """
-    str = ""
+    new_content = ""
     with open(filename) as fp:
         for txt in fp:
-            str += txt
-            for search_string in txt:
-                str += new_string
+            new_content += txt
+            if search_string in txt:
+                new_content += new_string
+                break
     with open(filename, "w") as fp2:
-        fp2.write(str)
+        fp2.write(new_content)
