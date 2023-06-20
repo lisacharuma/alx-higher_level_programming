d29ad80f3b0328d1a13e5cb5b1769a520fce7a71
lisacharuma <charumalisah@gmail.com>
2023-06-19 20:18:17 -0700

    Modified the update() method to handle kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,16 +95,28 @@ class Rectangle(Base):
         if self.__width == 0 or self.height == 0:
             print("")
 
-    def update(self, *args):
-        """Assigns args to attrs in order"""
-        num_args = len(args)
-        if num_args > 0:
-            self.id = args[0]
-        if num_args > 1:
-            self.__width = args[1]
-        if num_args > 2:
-            self.__height = args[2]
-        if num_args > 3:
-            self.__x = args[3]
-        if num_args > 4:
-            self.__y = args[4]
+    def update(self, *args, **kwargs):
+        """Assigns key-value args to attrs"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.__width = value
+                elif key == "height":
+                    self.__height = value
+                elif key == "x":
+                    self.__x = value
+                elif key == "y":
+                    self.__y = value
