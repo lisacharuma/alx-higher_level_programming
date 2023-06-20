82207c9cd02a4fd853c7917994127937ae13174f
lisacharuma <charumalisah@gmail.com>
2023-06-19 18:16:47 -0700

    Added a function that prints a pound rectangle in the Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,12 @@ class Rectangle(Base):
     def area(self):
         """Return rectangle area"""
         return (self.__width * self.__height)
+
+    def display(self):
+        """prints a # rectangle"""
+        for i in range(0, self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print("")
+        if self.__width == 0 or self.height == 0:
+            print("")
