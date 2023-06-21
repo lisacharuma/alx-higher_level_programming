71594769f2ccfa886d52ad399e99ad27539b90c0
lisacharuma <charumalisah@gmail.com>
2023-06-20 19:33:43 -0700

    Updated Base class by adding a function that writes str representation of list obj to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,3 +27,13 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Saves JSON string representation of list_obj to a file"""
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        j_str = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        with open(filename, 'w') as fp:
+            fp.write(j_str)
