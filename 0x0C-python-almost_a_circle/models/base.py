860621c47d99f9cfbefa214f3362ce6a1a5dbec8
lisacharuma <charumalisah@gmail.com>
2023-06-20 19:15:48 -0700

    Added a JSON to string function in the Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class module"""
+import json
 
 
 class Base:
@@ -18,3 +19,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Converts a list of dictionaries to a JSON string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
