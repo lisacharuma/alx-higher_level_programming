f007be0bfb86f74f80b8acf2fb27e90bb1faa195
lisacharuma <charumalisah@gmail.com>
2023-06-24 02:07:12 -0700

    Defined lookup() function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """A function that returns a list of available attrs and
+    methods of an object like the dir function"""
+    attributes = [attr for attr in dir(obj) if not attr.startswith("__")]
+    methods = [attr for attr in attributes if callable(getattr(obj, attr))]
+    return attributes + methods
