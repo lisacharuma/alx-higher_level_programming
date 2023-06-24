f9b261ff8f2ad57367578c2e31421d88c0740239
lisacharuma <charumalisah@gmail.com>
2023-06-24 02:29:10 -0700

    Fixed the return statement to return dir obj

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,7 +4,5 @@
 
 def lookup(obj):
     """A function that returns a list of available attrs and
-    methods of an object like the dir function"""
-    attributes = [attr for attr in dir(obj) if not attr.startswith("__")]
-    methods = [attr for attr in attributes if callable(getattr(obj, attr))]
-    return attributes + methods
+    methods of an object using the dir function"""
+    return dir(obj)
