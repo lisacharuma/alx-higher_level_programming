ef86b8b129cc316b7b6e6c9d34fb200c20522091
lisacharuma <charumalisah@gmail.com>
2023-06-25 04:31:23 -0700

    Added an area function to BaseGeometry that throws an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry:
+    """Base geometry template"""
+    def area(self):
+        """A function to calcualate area
+        raises an exception"""
+        raise Exception("area() is not implemented")
