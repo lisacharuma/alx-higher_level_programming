9874f7340b018fa3d45941acbd788a0ba1af2cf1
lisacharuma <charumalisah@gmail.com>
2023-06-27 03:06:39 -0700

    Fixed perimeter function to handle cases where height or width is 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,4 +38,7 @@ class Rectangle:
 
     def perimeter(self):
         """Returns rectangle parameters"""
-        return 2 * (self.width + self.height)
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
