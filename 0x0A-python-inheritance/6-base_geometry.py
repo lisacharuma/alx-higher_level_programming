24a729c4d73cbb0473026f3cd6b40345b7216c76
lisacharuma <charumalisah@gmail.com>
2023-09-11 09:00:44 -0700

    Added area method that raises an exception bcoz its nt implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry:
+    """BaseGeometry template"""
+    def area(self):
+        """A function to calculate area"""
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
