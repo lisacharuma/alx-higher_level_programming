b385c850ffce5b2593eb9e72b48b4c0d8bc7c07f
lisacharuma <charumalisah@gmail.com>
2023-09-16 04:40:13 -0700

    Removed a dir

--- 0x0C-python-almost_a_circle/models/6-test.py
+++
@@ -1,8 +0,0 @@
-def display(self):
-        """prints a # rectangle"""
-        for i in range(0, self.__height):
-            for j in range(self.__width):
-                print("#", end="")
-            print("")
-        if self.__width == 0 or self.height == 0:
-            print("")
