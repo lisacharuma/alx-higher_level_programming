042cdcd8220c6c5e3962c3d457492924551095df
lisacharuma <charumalisah@gmail.com>
2023-09-17 01:34:11 -0700

    Added a public method to_json to Student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class Student module"""
+
+
+class Student:
+    """Class representing a student"""
+    def __init__(self, first_name, last_name, age):
+        """New student obj initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a json rep of a student object
+        All attrs must be retrieved execpt when attrs
+        is a list of strings.
+        """
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            return {attr: getattr(self, attr) for attr
+                    in attrs if hasattr(self, attr)}
