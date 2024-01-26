e596cea8ecbdfe107bb4b4266208017d483a3a9c
lisacharuma <charumalisah@gmail.com>
2024-01-26 08:31:37 -0800

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 2. POST an email #0 """
+
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    _data = urlencode({'email': argv[2]}).encode('utf-8')
+    req = Request(url, data=_data)  # make method "POST"
+    with urlopen(req) as r:
+        html = r.read()
+        print('{}'.format(html.decode('utf-8')))
